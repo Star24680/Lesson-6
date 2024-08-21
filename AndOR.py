2a2d971fa4d789799e45cb27dd58d5e4046e4a8b
Star24680 <gulerumaan1@gmail.com>
2024-08-21 17:53:10 +0500

    first commit

---
+++ AndOR.py
@@ -0,0 +1,14 @@
+A=True
+B=True
+C=False
+if A and B and C:
+    print("All numbers have bullion value as true")
+else:
+    print("Atleast 1 number had bullion value false")
+A=10
+B=10
+C=0
+if A>0 or B>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater thsn 0")
